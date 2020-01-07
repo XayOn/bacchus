e2f710d80e22dc583734930d434ab7a14cf75c15
David Francos <xayon>
2020-01-07 23:56:23 +0100

    Added provider argument

--- src/bacchus/cli.py
+++ src/bacchus/cli.py
@@ -12,16 +12,15 @@ class GreetCommand(Command):
         {domain? : Domain (FQDN) for virtualhosts} 
         {username? : Nextcloud first user's username}
         {password? : Nextcloud first user's password}
+        {provider? : (Optional) Set up only one service} 
     """
     def handle(self):
         """Handle command"""
-        # TODO: More verbosity...
         setup = HomeServerSetup(domain=self.argument('domain'),
                                 nextcloud_username=self.argument('username'),
                                 nextcloud_password=self.argument('password'))
 
-        # TODO: Allow passing a specific provider
-        setup.configure()
+        setup.configure([self.argument('provider')])
 
 
 def main():
