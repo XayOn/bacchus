04aefffdc1d78f7972b8476bea1f69dcdb7d2e47
David Francos <xayon>
2020-02-16 22:24:56 +0100

    Added jackett preconfigured indexers

--- src/bacchus/jackett.py
+++ src/bacchus/jackett.py
@@ -1,5 +1,7 @@
 import json
+import shutil
 from .base import HomeServerApp
+from .base import TEMPLATES
 
 
 class Jackett(HomeServerApp):
@@ -7,6 +9,15 @@ class Jackett(HomeServerApp):
         self.container.stop()
         self.setup_nginx()
         self.compose.start()
+        self.copy_indexers()
+
+    def copy_indexers(self):
+        indexers_path = (self.path / 'Jackett' / 'Indexers')
+        indexers_path.mkdir(parents=True, exists_ok=True)
+
+        files = ((TEMPLATES / 'jackett' / 'Indexers').absolute()).glob('*')
+        for file in files:
+            shutil.copyfile(str(file.absolute()), indexers_path)
 
     def setup_nginx(self):
         config = self.path / 'Jackett' / 'ServerConfig.json'
