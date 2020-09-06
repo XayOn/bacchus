ccb1e6e20a4d6c2a2991023a7b6beccea4f29322
David Francos <xayon>
2020-09-07 01:02:33 +0200

    Added jinja2 templating to nginx
    This will allow us to make conditional nginx configuration, if a user
    doesn't want a specific service or service-group to be installed, we'll
    remove it from nginx config (as it wont be available)

--- src/bacchus/nginx.py
+++ src/bacchus/nginx.py
@@ -1,11 +1,13 @@
 from .base import HomeServerApp
 from .base import TEMPLATES
 from pathlib import Path
+from jinja2 import Template
 import subprocess
 
 
 class Nginx(HomeServerApp):
     def setup(self):
         self.logger.debug('Setting nginx configuration template')
-        (self.path / 'nginx.conf').write_text(
-            (TEMPLATES / 'nginx.tpl').read_text().format(domain=self.domain))
+        template = Template((TEMPLATES / 'nginx.tpl').read_text())
+        context = dict(domain=self.domain, )
+        (self.path / 'nginx.conf').write_text(template.render(**context))
