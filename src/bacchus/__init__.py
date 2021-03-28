c83dac96b9560e8d3702cd73595764e73f750caf
David Francos Cuartero <me@davidfrancos.net>
2021-03-28 01:45:39 +0100

    wip: #18 Write status files after a step is complete

--- src/bacchus/__init__.py
+++ src/bacchus/__init__.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from bacchus.arr import Radarr, Lidarr, Sonarr
 from bacchus.nextcloud import NextCloud
@@ -8,7 +9,9 @@ from bacchus.jellyfin import Jellyfin
 
 __all__ = [Jackett, Transmission, Lidarr, Radarr, Sonarr, Jellyfin, NextCloud]
 
+
 def main():
     for provider in __all__:
         getattr(provider(os.getenv('host')),
                 'setup_{os.getenv("step")}_step')()
+    Path('/data/bacchus_install_{os.getenv("step")}').write_text('')
